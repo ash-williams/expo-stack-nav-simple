4e2ef855bd433bab5b0cd6e02f0f118344013a51
Ashley Williams <55135751@ad.mmu.ac.uk>
2022-02-03 16:03:03 +0000

    Init commit

---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const AboutScreen = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>About Screen</Text>
+      </View>
+    );
+  }
+
+export default AboutScreen;
